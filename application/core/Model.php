e00364bc1d16d84c42d7f44a0c48002529ac3d7b
4ever-delayed <59034103+4ever-delayed@users.noreply.github.com>
2020-11-12 23:17:29 +0300

    Update Model.php

--- application/core/Model.php
+++ application/core/Model.php
@@ -5,7 +5,7 @@ class Model
     protected $db;
     protected $table;
 
-    function __construct(){
+    public function __construct(){
         $this->db = Database::getInstance();
         $this->table = 'guest_book';
 
